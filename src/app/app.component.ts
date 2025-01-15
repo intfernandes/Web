3f53a7a54d70164e873e12ef4921912544b28091
Lucas Fernandes <int2.fernandes@gmail.com>
2025-01-14 21:09:49 -0300

    adds http requests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'Web';
+export class AppComponent implements OnInit {
+  http = inject(HttpClient);
+  title = 'Constance';
+  users: any;
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/v1/users').subscribe({
+      next: (response) => (this.users = response),
+      error: (error) => console.log(error),
+      complete: () => console.log('Request has completed'),
+    });
+  }
 }
