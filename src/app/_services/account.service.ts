021b51f2256eacb4d2f2cb2d0b324d5b545ccaa9
Lucas Fernandes <int2.fernandes@gmail.com>
2025-01-24 18:10:20 -0300

    wire up login and signup

---
+++ src/app/_services/account.service.ts
@@ -0,0 +1,41 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable, signal } from '@angular/core';
+import { User } from '../_models/user';
+import { map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AccountService {
+  private http = inject(HttpClient);
+  baseUrl = 'https://localhost:5001/api/v1/';
+  currentUser = signal<User | null>(null);
+
+  login(model: any) {
+    return this.http.post<User>(this.baseUrl + 'auth/login', model).pipe(
+      map((user) => {
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUser.set(user);
+        }
+      })
+    );
+  }
+
+  register(model: any) {
+    return this.http.post<User>(this.baseUrl + 'auth/register', model).pipe(
+      map((user) => {
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUser.set(user);
+        }
+        return user;
+      })
+    );
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+    this.currentUser.set(null);
+  }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,29 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavComponent } from './nav/nav.component';
+import { AccountService } from './_services/account.service';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet ],
+  imports: [NavComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
-  http = inject(HttpClient);
-  title = 'Constance';
-  users: any;
+  private accountService = inject(AccountService);
 
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/v1/users').subscribe({
-      next: (response) => (this.users = response),
-      error: (error) => console.log(error),
-      complete: () => console.log('Request has completed'),
-    });
+    this.setCurrentUser();
+  }
+
+  setCurrentUser() {
+    const userStr = localStorage.getItem('user');
+
+    if (!userStr) return;
+
+    const user = JSON.parse(userStr);
+
+    this.accountService.currentUser.set(user);
   }
 }
